55caf510227376e9ecfc71eccb8a3a610c4ce35b
rahulshukla <rahul.shukla90@outlook.com>
2021-09-30 12:49:39 -0700

    Commiting Homework 1

--- HW-1/src/edu/northeastern/rahul/Main.java
+++ HW-1/src/edu/northeastern/rahul/Main.java
@@ -1,6 +1,10 @@
 package edu.northeastern.rahul;
 
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Hashtable;
+
 public class Main {
 
     public static void main(String[] args) {
@@ -8,25 +12,26 @@ public class Main {
         //Question 1
         int [] arr = {2,7,5,11,15};
         int target = 9;
-        sumTotal(arr,target);// calling for sumTotal function
+        sumTotal(arr,target);
 
         //Question 2
         int [] array = {-4,-1,0,3,10};
-        squareArray(array); //calling for squareArray function
-        bubbleSort(array); //calling for bubbleSort function
-        print(array); //calling for print function
+        int[] result = squareArray(array);
+        print(result); //calling for print function
 
         //Question 3
         int arr1[] = { 2,3,4,2,2,4,3,3,5,5,7 };
-        nonRepeating(arr1);// calling for nonRepeating funtion
+        int firstNonRepeating = findFirstNonRepeating( arr1 );
+        System.out.println("Answer 3: " + firstNonRepeating);
 
         //Question 4
-        System.out.println(construct("aaab", "aaz"));//calling for construct function
+        boolean isRansomPossible = canWeConstructRansomNote("aaab", "aaz");
+        System.out.println("Answer 4: " + isRansomPossible);
 
         //Question 5
-        String ag = "1A3d4s5t";
-        System.out.println("Answer 5: Lenght of string is " + ag.length());
-        checkDig(ag);//calling for checkDig function
+        String str = "1A3d4s5t";
+        boolean isCharFollowedByDigit = checkIfCharIsFollowedByDigit(str);
+        System.out.println("Answer 5: " + isCharFollowedByDigit);
 
     }//End of main function
 
@@ -40,7 +45,7 @@ public class Main {
                 System.out.println("Answer 1: Number is " + arr[start] + " & " + arr[end]);
                 return;
             }
-            if (arr[start] + arr[end] < target){
+            else if (arr[start] + arr[end] < target){
                 ++ start;
             } else {
                 -- end;
@@ -49,89 +54,93 @@ public class Main {
     }//End sumTotal function --> End of Question 1
 
     //Question 2
-    private static void squareArray(int[] array){
-        for( int i = 0; i < array.length; i++){
-            array[i] = array[i] * array[i];
-        }//End of for loop integer i
-    }//End of squareArray function
 
-    private static void bubbleSort(int[] array) {
-        for (int i = 0; i < array.length; i++) {
-            for (int j = 0; j < array.length - i - 1; j++) {
-                if (array[j] > array[j + 1]) {
-                    int k = array[j];
-                    array[j] = array[j + 1];
-                    array[j + 1] = k;
-                }//End of if statement
-            }//End of for loop integer j
-        }//End of for loop integer i
-    }//End of bubbleSort function
+    public static int[] squareArray(int[] array){
+        int[] result = new int[ array.length];
+        int start = 0;
+        int end = array.length - 1;
 
-    private static void print(int [] arr){
+        for (int i = array.length - 1; i >= 0; i--) {
+            int square;
+            if (Math.abs(result[start]) < Math.abs(result[end])) {
+                square = array[end];
+                end--;
+            } else {
+                square = array[start];
+                start++;
+            }
+            result[i] = square * square;
+        }
+        return result;
+    } //End of squareArray function
+
+    private static void print( int [] result){
         System.out.println("Answer 2: Output is ");
-        for (int j : arr) {
-            System.out.print(" " + j);
+        for (int i = 0 ; i < result.length ; i++) {
+            System.out.print(" " + result[i]);
         }//End of for loop integer i
+        System.out.println("");
     }//End of print Function --> End of Question 2
 
     //Question 3
-    private static void  nonRepeating(int arr[])
-    {
-        System.out.println("");
-        for (int i = 0; i < arr.length ; i++) {
-            int j;
-            for (j = 0; j <= arr.length; j++) {
-                if (j == arr.length)
-                {
-                    System.out.println("Answer 3: First non-repeating element is: "+arr[i]);
-                    return;
-                }//End of if statement
-                if (j != i && arr[i] == arr[j])
-                {
-                    break;
-                }//End of if statement
-            }//End of for loop integer j
-        }//End of for loop integer i
-        System.out.println("Answer 3: No Output found of non Repeating ");
-    }//End nonRepeating function --> End of Question 3
 
-    //Question 4
-    public static boolean construct(String ransomNote, String magazine) {
-        System.out.print("Answer 4: Output is ");
-        if (ransomNote.length() < magazine.length()) {
-            //Compairing the lenght of ransomNote & magazine
+    public static int findFirstNonRepeating(int[] array){
+        if (array == null || array.length == 0){
+            return Integer.MIN_VALUE;
+        }
+        HashSet<Integer> set = new HashSet<>();
+        for (Integer i : array) {
+            if(set.contains(i)){
+                return i;
+            } else {
+                set.add(i);
+            }
+        }
+        return Integer.MIN_VALUE;
+    } //End of findFirstNonRepeating function --> End of Question 3
+
+
+    // Question 4
+
+    public static boolean canWeConstructRansomNote(String ransomNote, String magazine){
+        if (magazine.length() < ransomNote.length()){
             return false;
-        }//End of if statement
-
-        boolean flag = false;
-        boolean visited[] = new boolean[ransomNote.length()];//Creating Array of type boolean
-        Arrays.fill(visited, Boolean.FALSE);//Assigning false value to boolean array
-        for (int i = 0; i < magazine.length(); i++) {
-            flag = false;
-            for (int j = 0; j < ransomNote.length(); j++) {
-                if (magazine.charAt(i) == ransomNote.charAt(j) && !visited[j]) {
-                    visited[j] = true;
-                    flag = true;
-                    break;
-                }//End of if statement
-            }//End for ransomeNote for loop
-        }//End for magaine for loop
-        return flag;
-    }//End construct function --> End of Question 4
+        }
+        HashMap<Character , Integer> hashMap = new HashMap<>();
+        for ( Character ch : magazine.toCharArray() ) {
+            if ( hashMap.containsKey(ch) ) {
+                hashMap.put( ch , hashMap.get(ch).intValue() + 1 );
+            } else {
+                hashMap.put(ch , 1 );
+            }
+        } //End of For loop magazine
+        for ( Character ch : ransomNote.toCharArray() ) {
+            if ( hashMap.containsKey(ch) ) {
+                hashMap.put( ch , hashMap.get(ch).intValue() - 1 );
+                if ( hashMap.get(ch).intValue() < 0 ){
+                    return false;
+                }
+            } else {
+                return false;
+            }
+        } //End of For loop ransomNote
+        return true;
+    }//End canWeConstructRansomNote function --> End of Question 4
 
     //Question 5
-    public static void checkDig(String ag) {
-        for (int i = 0; i <= ag.length() - 1; i++) {
-            int a = ag.charAt(i);//Getting ASCII value of alphabet
-            char b = ag.charAt(i + 1);//Getting digit
-            if (a >= 65 && a <= 122 && b >= '0' && b <= '9') {
-                i += 1;
+    public static boolean checkIfCharIsFollowedByDigit(String str) {
+        if (str == null || str.length() <= 1){
+            return true;
+        }
+        boolean isDigit = Character.isDigit( str.charAt(0) );
+        for ( int i = 1; i < str.length(); i++){
+            if ( isDigit == Character.isDigit( str.charAt(i) ) ) {
+                return false;
             } else {
-                System.out.println("Answer 5: Result is false");
-                return;
-            }//End of else statement
-        }//End of for loop integer i
-        System.out.println("Answer 5: Result is True");
-    }//End of checkDig function --> End of Question 5
+                isDigit = !isDigit;
+            }
+        }
+        return true;
+    } //End of checkIfCharIsFollowedByDigit -> End of Question 5
 
-}//End of class main
+} //End of class main
