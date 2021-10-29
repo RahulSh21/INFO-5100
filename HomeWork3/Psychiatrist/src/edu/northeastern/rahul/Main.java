0d91a880d7561debf4a1738cd3c484713ba94b9a
rahulshukla <rahul.shukla90@outlook.com>
2021-10-29 16:33:33 -0700

    Question3

--- HomeWork3/Psychiatrist/src/edu/northeastern/rahul/Main.java
+++ HomeWork3/Psychiatrist/src/edu/northeastern/rahul/Main.java
@@ -3,14 +3,14 @@ package edu.northeastern.rahul;
 public class Main {
 
     public static void main(String[] args) {
-        Psychiatrist p1 = new Psychiatrist();
+        Psychiatrist psychiatrist = new Psychiatrist();
 
         Moody moodyHappy = new Happy();
         Moody moodySad = new Sad();
-        p1.examine( moodyHappy);
-        p1.observe(moodyHappy);
+        psychiatrist.examine( moodyHappy);
+        psychiatrist.observe(moodyHappy);
         System.out.println();
-        p1.examine( moodySad);
-        p1.observe(moodySad);
+        psychiatrist.examine( moodySad);
+        psychiatrist.observe(moodySad);
     }
 }
