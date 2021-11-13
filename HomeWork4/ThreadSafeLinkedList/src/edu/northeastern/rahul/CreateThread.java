eb242dc6d02a9a2e2acf4f4de392f0ae9f0be92c
rahulshukla <rahul.shukla90@outlook.com>
2021-11-12 23:01:40 -0800

    ThreadSafeLinkList

--- HomeWork4/ThreadSafeLinkedList/src/edu/northeastern/rahul/CreateThread.java
+++ HomeWork4/ThreadSafeLinkedList/src/edu/northeastern/rahul/CreateThread.java
@@ -17,7 +17,7 @@ public class CreateThread implements Runnable{
             }
 
             try {
-                Thread.sleep(500);
+                Thread.sleep(1000);
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
